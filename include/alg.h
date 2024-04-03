faaeed528f4869687303aa5a8adf55403e12a414
Sabashnikov <160218448+Sabashnikov@users.noreply.github.com>
2024-04-03 23:27:40 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -2,9 +2,59 @@
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 
-#include <string>
+typedef char type;
+const int size = 100;
+class SStack {
+private:
+  type arr[size];
+  int top;
+public:
+  SStack() : top(-1) { }
+  type get() const {
+    return arr[top];
+  }
+  bool isEmpty() const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == size-1;
+  }
+  void pop() {
+    if (top >= 0)
+      top--;
+  }
+  void push(type item) {
+    if (top < size-1)
+      arr[++top] = item;
+  }
+};
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
+typedef int type1;
+class Stack {
+private:
+  type1 arr[size];
+  int top;
+public:
+  Stack() : top(-1) { }
+  type1 get() const {
+    return arr[top];
+  }
+  bool isEmpty() const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == size - 1;
+  }
+  void pop() {
+    arr[top] = 0;
+    if (top >= 0)
+      top--;
+  }
+  void push(type1 item) {
+    if (top < size - 1)
+      arr[++top] = item;
+  }
+};
 
-#endif  // INCLUDE_ALG_H_
+
+#endif  // INCLUDE_TSTACK_H_
