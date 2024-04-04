c6daa2821ed2488cadd6f2b1bd024fd02945898a
Sabashnikov <160218448+Sabashnikov@users.noreply.github.com>
2024-04-04 19:10:39 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,15 +2,15 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-typedef char type;
-const int size = 100;
+template<typename T, int size>
 class SStack {
 private:
-  type arr[size];
+  const int SIZE = size;
+  T arr[100];
   int top;
 public:
   SStack() : top(-1) { }
-  type get() const {
+  T get() const {
     return arr[top];
   }
   bool isEmpty() const {
@@ -23,38 +23,10 @@ public:
     if (top >= 0)
       top--;
   }
-  void push(type item) {
+  void push(T item) {
     if (top < size-1)
       arr[++top] = item;
   }
 };
-
-typedef int type1;
-class Stack {
-private:
-  type1 arr[size];
-  int top;
-public:
-  Stack() : top(-1) { }
-  type1 get() const {
-    return arr[top];
-  }
-  bool isEmpty() const {
-    return top == -1;
-  }
-  bool isFull() const {
-    return top == size - 1;
-  }
-  void pop() {
-    arr[top] = 0;
-    if (top >= 0)
-      top--;
-  }
-  void push(type1 item) {
-    if (top < size - 1)
-      arr[++top] = item;
-  }
-};
-
 
 #endif  // INCLUDE_TSTACK_H_
